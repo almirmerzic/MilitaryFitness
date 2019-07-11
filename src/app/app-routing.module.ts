553746cfbcfb0d8e2ff95b5456bdcc16994f998f
almirmerzic <33768032+almirmerzic@users.noreply.github.com>
2019-07-11 15:43:51 +0200

    Routes  component imported and working on home page
    Routes  component imported and working on home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutusComponent } from './components/aboutus/aboutus.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { ParticipateComponent } from './components/participate/participate.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutusComponent },
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: 'participate', component: ParticipateComponent }
 
 ];
 
